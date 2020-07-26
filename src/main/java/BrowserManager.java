96cbedc71f4a71a7a534091d94fd69c387d4e34d
Neha Patel <nehajp999@gmail.com>
2020-07-26 12:45:53 +0100

    Added Giftcard

--- src/main/java/BrowserManager.java
+++ src/main/java/BrowserManager.java
@@ -1,5 +1,5 @@
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -7,12 +7,18 @@ public class BrowserManager extends Util1 {
 
     public  void SetBrowser() {
 
-        System.setProperty("webdriver.chrome.driver", "src\\test\\BrowserDriver\\chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("useAutomationExtension", false);
-        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+//        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\BrowserDriver\\chromedriver.exe");
+//        ChromeOptions options = new ChromeOptions();
+//        options.setExperimentalOption("useAutomationExtension", false);
+//        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+//        driver = new ChromeDriver(options);
+ //       System.setProperty("webdriver.gecko.driver", "src\\test\\resources\\BrowserDriver\\geckodriver.exe");
+
+ //       driver = new FirefoxDriver();
+
+        System.setProperty("webdriver.ie.driver", "src\\test\\resources\\BrowserDriver\\IEDriverServer.exe");
+        WebDriver driver = new InternetExplorerDriver();
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         driver.get("https://demo.nopcommerce.com/");
         driver.manage().window().maximize();
         }
